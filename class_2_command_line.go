93ceebcd524130d111399fc229f4460778303f4c
ChairChaos <asdingochaos@163.com>
2022-01-29 14:32:29 +0800

    添加命令行输入

---
+++ class_2_command_line.go
@@ -0,0 +1,29 @@
+/*
+*       Create on : 2022/1/29 10:56
+*       @Author : czx
+ */
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+	fmt.Println(os.Args)
+}
+
+func echo2() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
